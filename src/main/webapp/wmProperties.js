354edf4a62e107559de50a2c95483a40b6cf1c4d
ZMlWxmArSN <automate1@wavemaker.com>
2017-11-16 18:29:15 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_kAvlnipOzA",
+  "homePage" : "Main",
+  "name" : "Auto_kAvlnipOzA",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
